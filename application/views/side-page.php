d59bd89bdb84f798c6df904228f0d182326731b5
WereDouglas <weredouglas@gmail.com>
2016-05-10 10:48:57 +0300

    added changes to all things but this

--- application/views/side-page.php
+++ application/views/side-page.php
@@ -90,10 +90,9 @@
 
             </a>          
         </li> 
-        <li> <a href="<?php echo base_url() . "files/Cp.rar"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>Desktop(rar)</b> </a> </li>
-        <li> <a href="<?php echo base_url() . "files/Cp.zip"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>Desktop (zip)</b> </a> </li>
-        <li> <a href="<?php echo base_url() . "files/NET4.5.1.rar"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>NET 4.5.1(rar)</b> </a> </li>
-        <li> <a href="<?php echo base_url() . "files/NET4.5.1.zip"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>NET 4.5.1(rar)</b> </a> </li>
+        <li> <a href="<?php echo base_url() . "files/Cp.msi"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>Desktop(exe)</b> </a> </li>
+
+        <li> <a href="<?php echo base_url() . "files/NET4.5.1.exe"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>NET 4.5.1(exe)</b> </a> </li>
 
 
         <li> <a href="<?php echo base_url() . "files/Cp.apk"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>Mobile</b> </a> </li>
